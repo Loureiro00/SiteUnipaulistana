1b89b93b369580e33a3845c8900c645f8685583d
Loureiro00 <pedrohstloureiro@gmail.com>
2022-10-21 20:32:41 -0300

    Ok!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-function mensagem(){
-    alert("Braço curto !!!!")
-}
+function validacao(){
+    let usuarioValido = "Pedro";
+    let senhaValida = "123456";
+    
+        let usuario =document.getElementById("usuario").value;
+        
+        let senha = document.getElementById ("senha").value;
+        
+    //usuario vazio
+        if (usuario == "" && senha ==""){
+            alert ("Não foi possivel logar, usuario e senha vazio");
+        }
+        else if (usuario == "" && senha == senhaValida){
+            alert ("Não foi possivel logar, por favor preencha o usuario");
+        }
+        else if ( usuario == "" && senha !=senhaValida){
+            alert ("Não foi possivel logar, senha incorreta e usuario em branco");    
+        }
+
+                 
+       
+        else if( usuario == usuarioValido && senha == senhaValida){
+            alert(" Usuario valido !!");
+        }
+    }
